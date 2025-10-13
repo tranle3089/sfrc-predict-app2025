78694f4eb4ab5758bdd6d9808861b69c438bed89
tranle3089 <huyenle3089@gmail.com>
2025-10-13 17:20:06 +0900

    Update section_models.py

--- section_models.py
+++ section_models.py
@@ -3,19 +3,18 @@ import streamlit as st
 def show():
     diagram = """
 Machine Learning Model Selection
-│
-├── Regression based algorithms
-│     ├── Adaboost
-│     ├── Catboost
-│     └── XGboost
-│
-├── Tree based algorithms
-│     ├── Extra tree
-│     └── Random forest
-│
-└── Neural based algorithm
-      └── Artificial Neural Network
+
+- Regression based algorithms
+    - Adaboost
+    - Catboost
+    - XGboost
+
+- Tree based algorithms
+    - Extra tree
+    - Random forest
+
+- Neural based algorithm
+    - Artificial Neural Network
 """
 
-    # Hiển thị sơ đồ chữ trong Streamlit
     st.markdown(f"```text\n{diagram}\n```")
